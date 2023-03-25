2cb8c3d92b52bf445aa977375be8155f7997a02b
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-25 22:24:58 +0530

    User table created

---
+++ Leave Request Application/Data/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Leave_Request_Application.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? RegisterId { get; set; }
+
+        public string NIC { get; set; }
+
+    }
+}
