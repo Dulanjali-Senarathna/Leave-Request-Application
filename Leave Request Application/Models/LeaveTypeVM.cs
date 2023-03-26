f4f6eaaa68dc5315702528a6975db06d8f06c668
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-26 19:18:56 +0530

    Create view done

--- Leave Request Application/Models/LeaveTypeVM.cs
+++ Leave Request Application/Models/LeaveTypeVM.cs
@@ -6,10 +6,14 @@ namespace Leave_Request_Application.Models
     {
         public int id {  get; set; }
 
+        
         [Display(Name = "Leave Type Name")]
+        [Required]
         public string Name { get; set; }
 
         [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please Enter a Valid Number")]
         public int DefaultDays { get; set; }
     }
 }
