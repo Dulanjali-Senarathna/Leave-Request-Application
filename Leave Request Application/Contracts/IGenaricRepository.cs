e97941ef0b62a568a97ea00c4deb5c9e1e63b421
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-27 14:34:32 +0530

    Created generic interface

---
+++ Leave Request Application/Contracts/IGenaricRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Leave_Request_Application.Contracts
+{
+    //creating genaric interface
+    public interface IGenaricRepository<T> where T : class //T has to me a class
+    {
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task <int> GetCountAsync();
+        Task<bool> Exists(int id);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(T entity);
+
+    }
+}
